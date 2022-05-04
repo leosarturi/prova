cb3d4fa2852afdaffbaf46040fb14001ae7bf981
leosarturi <sarturilazier@gmail.com>
2022-05-04 01:08:54 -0300

    Update ListaController.ts

--- prova/app2/src/controller/ListaController.ts
+++ prova/app2/src/controller/ListaController.ts
@@ -4,14 +4,16 @@
  const listaprocessada : folhaPagamento[] = [];
 export class ListaController{
     consultar(request:Request, response:Response){
-        var dataret;
-        var mess;
+      
 
                 
-                const url= "http://localhost:3333/folha/calcular";
-      var ret;
-      var folha : folhaPagamento[];
-       axios.get(url).then((data)=>{
+    const url= "http://localhost:3333/folha/calcular";
+    var ret;
+    var folha : folhaPagamento[];
+
+
+    
+    axios.get(url).then((data)=>{
            
            ret = data.data;
            
@@ -21,18 +23,17 @@ export class ListaController{
                     if(dados.ano == parseInt(request.params.ano) && dados.mes == parseInt(request.params.mes) && dados.funcionario.cpf == request.params.cpf){
                         listaprocessada.push(dados);
                         
-                        response.status(200).json({message:"sucesso" , data:"dados"})
+                        response.status(200).json({message:"sucesso" , data:dados});
                     }
                 else {
-                    mess="erro";
-                    dataret = "não foi possivel encontrar";
+                    
+                    response.status(200).json({message:"erro" , data:"não foi possível encontrar"});
                 }
-           
-       
-           
-       
-    }
-}).catch();
+            })
+        }).catch();
+        
+            
+        
     }
     listar(request:Request, response:Response){
         var salario =0;
@@ -40,10 +41,36 @@ export class ListaController{
         var folha:folhaPagamento[];
         axios.get(url).then((data)=>{
 folha = data.data.data;
-folha.forEach(data=>{
-    salario+=data.bruto;
-})
+if(listaprocessada.length>0){
+
+    
+    listaprocessada.forEach((dados)=>{
+        folha.forEach((data,index)=>{
+        
+        if(data.funcionario.cpf!=dados.funcionario.cpf || data.ano != dados.ano || data.mes != dados.mes){
+            
+            salario+=data.bruto;
+            listaprocessada.push(data);
+            return data;
+        }else{
+            folha.splice(index,1);
+        }
+       
+    })
+    
+    
+})}else{
+    folha.forEach(data=>{
+        salario+=data.liquido;
+        listaprocessada.push(data);
+    })
+}
+
+if(folha.length==0){
+    response.status(200).json({message:"erro" , data:"todos as folhas já processadas"})    
+}else{
 response.status(200).json({message:"sucesso" , data:folha,salariototal:salario})
+}
         }).catch();
     }
 
